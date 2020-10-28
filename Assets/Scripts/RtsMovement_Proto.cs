310a8326e4ca7ef0f470981c1413406e7775a8bf
Tight-Head <73498454+Tight-Head@users.noreply.github.com>
2020-10-28 14:58:17 +0000

    Moving capsule to node
    Literally just a simple test to move a capsule to a node.
    Im not sure how to upascale it to multiple units/nodes however.

---
+++ Assets/Scripts/RtsMovement_Proto.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RtsMovement_Proto : MonoBehaviour
+{
+
+    Transform unitPosition;
+    float movementSpeed;
+    public bool unitSelected;
+    bool unitMoving;
+    Canvas can;
+    public Image img;
+    Vector3 targetPosition;
+    RtsNode_Proto rtsNode;
+
+    void Start()
+    {
+        unitSelected = false;
+        unitMoving = false;
+        can = GetComponent<Canvas>();
+        img.enabled = false;
+        movementSpeed = 10f;
+        rtsNode = GameObject.FindGameObjectWithTag("Node").GetComponent<RtsNode_Proto>();
+    }
+
+    private void OnMouseOver()
+    {
+        img.enabled = true;
+    }
+
+    private void OnMouseExit()
+    {
+        if (unitSelected == false)
+        {
+            img.enabled = false;
+        }
+        else
+        {
+            img.enabled = true;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (gameObject.tag == "Unit")
+        {
+            unitSelected = true;
+        }
+       
+    }
+
+    private void Update()
+    {                     
+        if(rtsNode.nodeSelected == true)
+        {
+            targetPosition = rtsNode.nodePos;
+        }
+
+         if(unitSelected == true && rtsNode.nodeSelected == true)
+        {
+            this.gameObject.transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * movementSpeed);
+            unitMoving = true;
+        }
+
+    }
+
+
+}
---
+++ Assets/Scripts/RtsNode_Proto.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RtsNode_Proto : MonoBehaviour
+{
+
+    RtsMovement_Proto rtsMove;
+    public bool nodeSelected;
+    public Vector3 nodePos; 
+
+    private void OnMouseDown()
+    {
+        if (rtsMove.unitSelected == true)
+        {
+            nodeSelected = true;
+        }
+    }
+
+
+    private void Start()
+    {
+        nodePos = gameObject.transform.position;
+        rtsMove = GameObject.FindGameObjectWithTag("Unit").GetComponent<RtsMovement_Proto>();
+    }
+}
