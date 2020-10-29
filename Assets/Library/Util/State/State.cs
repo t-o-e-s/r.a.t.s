ef11eb125f662178c22d15d91ff4085ba14ce557
Samuel <samwis3morgan@gmail.com>
2020-10-29 19:03:07 +0000

    wip

---
+++ Assets/Library/Util/State/State.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State
+{
+    private Statuses[] statuses;
+
+    private GameObject gameObject;
+
+    private int interval;
+
+    protected State(GameObject gameObject, int interval)
+    {
+        this.gameObject = gameObject;
+        this.interval = interval;
+    }
+}
---
+++ Assets/Library/Util/State/UnitState.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+public class UnitState : State
+{
+    public UnitState(GameObject gameObject, int interval) : base(gameObject, interval)
+    {
+
+    }
+}
