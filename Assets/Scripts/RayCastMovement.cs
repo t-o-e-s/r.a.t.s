7eff906869325432f64f765399b6566ad7ee0bec
Samuel <samwis3morgan@gmail.com>
2020-10-31 12:50:45 +0000

    added new movement system

---
+++ Assets/Scripts/RayCastMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class RayCastMovement : MonoBehaviour
+{
+    GameObject unit;
+
+    void Awake()
+    {
+        unit = GameObject.Find("MainUnit");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100f))
+        {
+            if (hit.collider.CompareTag("movement_tile"))
+            {
+                Vector3 v = new Vector3(
+                    hit.collider.gameObject.transform.position.x, 0, 
+                    hit.collider.gameObject.transform.position.z);
+
+                unit.GetComponent<NavMeshAgent>().SetDestination(v);
+            }
+
+        }
+    }
+}
