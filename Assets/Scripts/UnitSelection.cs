9f20b5d4c408c99a12fc397f174e5e16d1b2ad13
Tight-Head <73498454+Tight-Head@users.noreply.github.com>
2020-11-03 19:38:20 +0000

    Update UnitSelection.cs

--- Assets/Scripts/UnitSelection.cs
+++ Assets/Scripts/UnitSelection.cs
@@ -68,17 +68,7 @@ public class UnitSelection : MonoBehaviour
         }
     }
 
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(agent.GetAreaCost == 10)
-        {
-            agent.speed = 5;
-        }
-        if(agent.GetAreaCost == 1)
-        {
-            agent.speed = 10;
-        }
-    }
+ 
 }
