c792eb93cbc4c97d0693f57a5680aad8b1a56493
Samuel <samwis3morgan@gmail.com>
2020-11-03 20:58:56 +0000

    stash

---
+++ Assets/Library/Scripts/Units/UnitController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitController : MonoBehaviour, IUnit
+{
+    NavMeshAgent agent;
+
+    public void Move(Vector3 target)
+    {
+        agent.SetDestination(target);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
