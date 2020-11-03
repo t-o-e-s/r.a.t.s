1cc5de3684d6ce2f28285272852ea81ee36c98d1
Tight-Head <73498454+Tight-Head@users.noreply.github.com>
2020-11-03 13:18:40 +0000

    Units can be individually or group selected for movement

--- Assets/Scripts/RayCastMovement.cs
+++ Assets/Scripts/RayCastMovement.cs
@@ -5,21 +5,44 @@ using UnityEngine.AI;
 
 public class RayCastMovement : MonoBehaviour
 {
-    GameObject unit;
 
+    GameObject[] units;
     void Awake()
     {
-        unit = GameObject.Find("MainUnit");
-        
+        units = GameObject.FindGameObjectsWithTag("Unit");              
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)) HandleMovement();
+       // if (Input.GetMouseButtonDown(0)) 
+            //HandleMovement();
+
+        foreach (GameObject unit in units)
+        {
+  
+            if (unit.GetComponent<UnitSelection>().unitSelected == true && Input.GetMouseButtonDown(0))
+            {
+                RaycastHit hit;
+
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100f))
+                {
+                    if (hit.collider.CompareTag("movement_tile"))
+                    {
+                        Vector3 v = new Vector3(
+                            hit.collider.gameObject.transform.position.x, 0,
+                            hit.collider.gameObject.transform.position.z);
+
+                        unit.GetComponent<NavMeshAgent>().SetDestination(v);
+                    }
+
+                }
+            }
+
+        }
     }
 
-    void HandleMovement()
+   /* void HandleMovement()
     {
         RaycastHit hit;
 
@@ -35,5 +58,5 @@ public class RayCastMovement : MonoBehaviour
             }
 
         }
-    }
+    }*/
 }
---
+++ Assets/Scripts/UnitSelection.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitSelection : MonoBehaviour
+{
+    public Image selectInd;
+    public bool unitSelected;
+
+    private void Awake()
+    {
+        selectInd.enabled = false;
+        unitSelected = false;
+        selectInd = GetComponentInChildren<Image>();
+    }
+
+    private void OnMouseOver()
+    {
+        if (unitSelected == false)
+        {
+            selectInd.enabled = true;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (unitSelected == false)
+        {
+            selectInd.enabled = false;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (unitSelected == false)
+        {
+            selectInd.enabled = true;
+            unitSelected = true;
+        }
+        else
+        {
+            selectInd.enabled = false;
+            unitSelected = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
