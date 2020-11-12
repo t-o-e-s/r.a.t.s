eb0fe5a90cd363d85b1fda28fe81cc794e206abc
Tight-Head <73498454+Tight-Head@users.noreply.github.com>
2020-11-12 14:38:11 +0000

    proto of timeMech now includes basic raising of bridge

---
+++ Assets/Library/Units/BridgeHandler.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BridgeHandler : MonoBehaviour
+{
+    public bool bridgeUp;
+    public GameObject bridge;
+
+    private void Update()
+    {
+        if(bridge.transform.position.y >= -0.8f)
+        {
+            bridgeUp = true;
+        }
+    }
+
+
+    public void RewindBridge()
+    {
+        if (!bridgeUp)
+        {
+            bridge.transform.position += new Vector3(0, 1 * Time.deltaTime, 0);
+        }
+    }
+}
---
+++ Assets/Library/Units/TimeMech.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeMech : MonoBehaviour
+{
+    public TimeHandler timeH;
+    BridgeHandler bridgeH;
+
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        bridgeH = other.gameObject.GetComponent<BridgeHandler>();
+
+        if (timeH.isRewinding == true)
+        {
+            if (other.gameObject.tag == "timeTile" && bridgeH.bridgeUp == false)
+            {
+                bridgeH.RewindBridge();
+            }
+        }
+    }
+}
