ca0c1f50ea62ffaa6f6aaf8858300c5263765d1d
TallGuyScott <73498417+TallGuyScott@users.noreply.github.com>
2021-01-12 14:35:38 +0000

    Clock now pauses game correctly

--- Assets/Library/src/util/PauseControl.cs
+++ Assets/Library/src/util/PauseControl.cs
@@ -13,15 +13,17 @@ public class PauseControl : MonoBehaviour
         }
     }
 
-    void PauseGame()
+    public void PauseGame()
     {
         if (gameIsPaused)
         {
             Time.timeScale = 0f;
+            // would be nice if we could reduce timescale to 0 gradually over the course of one second or so instead of instantly
         }
         else
         {
             Time.timeScale = 1;
+            //same as above but getting back up to timescale of 1
         }
     }
 }
