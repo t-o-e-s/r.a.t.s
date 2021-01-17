c16de2acad5c6f724d4a217a4b7949d9c3642bd9
Samuel <samwis3morgan@gmail.com>
2021-01-17 22:00:51 +0000

    time sensitive items added

---
+++ Assets/Library/src/time/TimeSensitive.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using Library.src.time.records;
+using Library.src.units;
+using UnityEngine;
+
+namespace Library.src.time
+{
+    public abstract class TimeSensitive : MonoBehaviour
+    {
+        protected readonly List<Record> nextRecords = new List<Record>();
+        protected readonly List<Record> previousRecords = new List<Record>();
+        
+        protected Coroutine rewindRoutine;
+
+
+        public abstract void Record();
+        public abstract void PlayRecord(Record record);
+
+        public void Forward()
+        {
+            var index = nextRecords.Count - 1;
+            var record = nextRecords[index];
+            nextRecords.RemoveAt(index);
+            PlayRecord(record);
+
+        }
+
+        public void Rewind()
+        {
+            PlayRecord(PreviousRecord());
+        }
+
+        public  Record RecordUnit(Unit unit)
+        {
+            return new UnitRecord(unit);
+        }
+
+        public bool IsRewinding()
+        {
+            return rewindRoutine != null 
+                   && previousRecords.Count > 0;
+        }
+
+        protected Record PreviousRecord()
+        {
+            var index = previousRecords.Count - 1;
+            var record = previousRecords[index];
+            previousRecords.RemoveAt(index);
+            return record;
+        }
+
+        public void Stop()
+        {
+            StopCoroutine(rewindRoutine);
+            rewindRoutine = null;
+        }
+    }
+}
---
+++ Assets/Library/src/time/records/UnitRecord.cs
@@ -0,0 +1,31 @@
+﻿using Library.src.combat.Weapon;
+using Library.src.elements;
+using Library.src.units;
+using UnityEngine;
+
+namespace Library.src.time.records
+{
+    public class UnitRecord : Record
+    {
+        public readonly Vector3 position;
+        public readonly Weapon weapon;
+
+        public readonly Status[] statuses;
+
+        public readonly float health;
+        public readonly float speed;
+
+
+        public UnitRecord(Unit unit) : base(unit.controller.gameObject.GetInstanceID(), unit.name)
+        {
+            var t = unit.controller.transform.position;
+            position = new Vector3(t.x, t.y, t.z);
+            weapon = unit.weapon;
+
+            statuses = unit.statuses;
+
+            health = unit.health;
+            speed = unit.speed;
+        }
+    }
+}
